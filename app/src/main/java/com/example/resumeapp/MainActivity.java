ca2603720f31b6e4b7389f9692c44156fcc0a1e7
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-03-12 17:14:12 +0500

    Splash Screen

--- app/src/main/java/com/example/resumeapp/MainActivity.java
+++ app/src/main/java/com/example/resumeapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.resumeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView Resume;
+    TextView Builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+        Animation fade = AnimationUtils.loadAnimation(this,R.anim.fade);
+        Resume.startAnimation(fade);
+        Builder.startAnimation(fade);
+
+        // Move to Start activity after 4 seconds
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+            finish(); // Close the SplashActivity
+        }, 3000); // 3 seconds delay
+
+
+    }
+
+    public void init(){
+        Resume = findViewById(R.id.resume);
+        Builder = findViewById(R.id.builder);
     }
 }
