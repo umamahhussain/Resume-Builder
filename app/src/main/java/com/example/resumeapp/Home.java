e8a0b420415b626f47883cc04b821db93c3c8f3a
Umamah Hussain <152015569+umamahhussain@users.noreply.github.com>
2025-03-14 02:33:33 +0500

    Home

--- app/src/main/java/com/example/resumeapp/Home.java
+++ app/src/main/java/com/example/resumeapp/Home.java
@@ -1,6 +1,10 @@
 package com.example.resumeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
+
+    Button prof, exp, edu, cert, ref, sum, det;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,60 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+        Animation leftTOright = AnimationUtils.loadAnimation(this, R.anim.left_to_right);
+        Animation rightTOleft = AnimationUtils.loadAnimation(this, R.anim.right_to_left);
+        prof.startAnimation(leftTOright);
+        det.startAnimation(rightTOleft);
+        sum.startAnimation(leftTOright);
+        edu.startAnimation(rightTOleft);
+        exp.startAnimation(leftTOright);
+        cert.startAnimation(rightTOleft);
+        ref.startAnimation(leftTOright);
+
+        prof.setOnClickListener(v -> {
+            Intent profile = new Intent(this, ProfilePicture.class);
+            startActivity(profile);
+        });
+        det.setOnClickListener(v -> {
+            Intent details = new Intent(this, PersonalDetails.class);
+            startActivity(details);
+        });
+        sum.setOnClickListener(v -> {
+            Intent summary = new Intent(this, Summary.class);
+            startActivity(summary);
+        });
+        edu.setOnClickListener(v -> {
+            Intent education = new Intent(this, Education.class);
+            startActivity(education);
+        });
+        exp.setOnClickListener(v -> {
+            Intent experience = new Intent(this, Experience.class);
+            startActivity(experience);
+        });
+        cert.setOnClickListener(v -> {
+            Intent certifications = new Intent(this, Certifications.class);
+            startActivity(certifications);
+        });
+        ref.setOnClickListener(v -> {
+            Intent references = new Intent(this, References.class);
+            startActivity(references);
+        });
+
+
+
+
+    }
+
+    public void init()
+    {
+        prof = findViewById( R.id.btn_profile );
+        exp = findViewById( R.id.btn_exp );
+        cert = findViewById( R.id.btn_cert );
+        edu = findViewById( R.id.btn_edu );
+        ref = findViewById( R.id.btn_ref );
+        sum = findViewById( R.id.btn_summary );
+        det = findViewById( R.id.btn_persDet );
     }
 }
